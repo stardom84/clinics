6a898ba03013f6b73e5919969924a6a2bf558691
kyoungtaek <Subasuba008>
2016-12-01 18:05:46 +0700

    - implementing showing  geocode of clicked position on Google map

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {GoogleMapService} from '../../shared/google.map.service';
-import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'my-register',
@@ -10,9 +9,6 @@ import {Observable} from 'rxjs/Observable';
 
 export class RegisterComponent implements OnInit {
 
-  private googleMaps: any;
-  private map: any;
-  private onClickMapOb: (map: any, event: string) => Observable<{}>;
 
   constructor(
     private googleMapSrv: GoogleMapService
@@ -21,39 +17,9 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {
-    console.log('Hello register');
-    this.createMap();
-  }
+    this.googleMapSrv.createMap(document.getElementById('map'), {lat: 37.49736948554443, lng: 127.02452659606933});
 
-  private createMap() {
-    this.googleMapSrv.getApi().subscribe(googleMaps => {
-        this.googleMaps = googleMaps;
-        this.map = new googleMaps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: new googleMaps.LatLng(37.49736948554443, 127.02452659606933),
-          mapTypeId: googleMaps.MapTypeId.ROADMAP
-        });
-        console.log('this.map', this.map);
-        this.addEventListenerOnMap();
-      },
-      (err) => {
-        console.log('err!!', err);
-      },
-      () => {
-        console.log('completed');
-      });
-  }
-
-  private addEventListenerOnMap() {
-    this.onClickMapOb = Observable.bindCallback((map: any, event: string, cb: () => any) => {
-      this.googleMaps.event.addListener(map, event, cb);
-    });
-
-    this.onClickMapOb(this.map, 'click').subscribe(this.onClickMap);
-  }
-
-  private onClickMap(mouseEvent: any) {
-    console.log('mouseEvent', mouseEvent);
-    return mouseEvent;
+    this.googleMapSrv.getGeocodeByClickOb()
+        .subscribe((mouseEvent: google.maps.MouseEvent) => { console.log(mouseEvent); });
   }
 }
--- src/app/shared/google.map.service.ts
+++ src/app/shared/google.map.service.ts
@@ -5,35 +5,68 @@ import {AsyncSubject} from 'rxjs/AsyncSubject';
 export class GoogleMapService {
 
   private api: any;
+  private googleMaps: any;
   private apiKey = 'AIzaSyCTrSHR8nxKobRiroiMR65kFUhWx6SzYd0';
+  private map: google.maps.Map;
 
   constructor() {}
 
-  public getApi(language = 'en'): Observable<any> {
+  createMap(mapEl: HTMLElement, latLng: google.maps.LatLngLiteral) {
+    this.getApi().subscribe(
+      googleMaps => {
+        this.googleMaps = googleMaps;
+        this.map = new googleMaps.Map(mapEl, {
+          zoom: 12,
+          center: new googleMaps.LatLng(latLng.lat, latLng.lng),
+          mapTypeId: googleMaps.MapTypeId.ROADMAP
+        });
+        this.addEventListenerOnMap();
+      },
+      err => { console.log('err!!', err); },
+      () => { console.log('completed'); });
+  }
+
+  getGeocodeByClickOb(): Observable<google.maps.MouseEvent> {
+    return this.registerClickToGetGeocode();
+  }
+
+  private getApi(language = 'en'): Observable<any> {
     if (this.api) {
       return Observable.of<any>(this.api);
     }
 
-    const callbackName = '__googleMapsApiOnLoadCallback';
-    const subject = new AsyncSubject();
-    const params = [`callback=${callbackName}`];
-    const scriptElement = document.createElement('script');
+    const callbackName  = '__googleMapsApiOnLoadCallback',
+          params        = [],
+          subject       = new AsyncSubject(),
+          scriptElement = document.createElement('script');
 
+    params.push(`callback=${callbackName}`);
     params.push(`key=${this.apiKey}`);
-    if (language) { params.push(`language=${language}`); }
+    params.push(`language=${language}`);
 
     scriptElement.src = `https://maps.googleapis.com/maps/api/js?${params.join('&')}`;
 
     window[callbackName] = () => {
-      console.log('calling cb..');
+      console.log('Calling __googleMapsApiOnLoadCallback...');
+
       subject.next(window.google.maps);
       subject.complete();
+
       this.api = window.google.maps;
+
       delete window[callbackName];
     };
 
     document.body.appendChild(scriptElement);
 
     return subject;
+  }
+
+  private registerClickToGetGeocode(): Observable<google.maps.MouseEvent> {
+    const onClickMapOb = Observable.bindCallback((map: any, event: string, cb: () => any) => {
+      this.googleMaps.event.addListener(map, event, cb);
+    });
+
+    return onClickMapOb(this.map, 'click');
   }
 }
